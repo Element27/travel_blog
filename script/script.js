a18581357b926ebe7a9ee6b63d60927abb482b1a
Element27 <olasocials@gmail.com>
2025-10-21 12:26:08 +0100

    js begins

--- script/script.js
+++ script/script.js
@@ -1,137 +1,69 @@
+// default import
+import data from "./dummy.js";
+// name import
+import { sampleExpprt, anotherExpprt } from "./newScript.js";
 
-// Variables
+const feature_card_container = document.getElementById(
+  "feature_card_container"
+);
 
-// console.log(varNum)
+// a map takes an existing array formats/modifies and return a new array that fits the format/modification. Does not affect the original array
+const all_feature_loc = data
+  .map((item) => {
+    // string literals allow be able to parse in dynamic values into a string.
+    return `
+        <div class="feature_card" >
+          <img src=${item.image} />
+          <div class="feature_card_texts">
+            <h4>${item.title}</h4>
+            <p>
+              ${item.post.slice(0, 100)}
+            </p>
+          </div>
+        </div>
+    `;
+    // join is an array fcuntion that concatenates items in an array into a string, elemination the specified delimiter.
+  })
+  .join("");
 
-// // variable declaration and assignment
-// var varNum = 20;
+feature_card_container.innerHTML =
+  all_feature_loc +
+  ` <div class="feature_card">
+          <a class="add_new_blog" href="add-blog.html">
+            <h4>
+              <span class="material-symbols-outlined"> add </span>
+            </h4>
+            <p>New Blog</p>
+          </a>
+        </div>`;
 
-// num or intergers
-let letNum = 30;
-// console.log(letNum)
+// console.log("all_feature_loc", all_feature_loc);
+// console.log("feature_card_container", feature_card_container.innerHTML);
 
-letNum = 35;
-// console.log(letNum)
+// JavaScript object notation
 
-// const is short for constant
-const constNum = 40;
+// Common & Modular
 
-// console.log(constNum)
+const tsArray = [1, 2, 3, 4, 5];
+console.log("before", tsArray);
 
-// - infinity to + infinity
-// int 
-// /bigInt
+const thisArray = [];
+// 1a,2a,3a,4a,5a
 
-// float
-
-
-// String
-// alphanumeric characters
-
-const myName = "Daniel";
-const nickNmae = "Danny-1-1";
-
-
-const the = myName.slice(2)
-console.log(the)
-const tex = "21";
-// const xet = 20;
-
-// console.log(tex + xet);
-// console.log(tex - xet);
-// console.log(tex * xet);
-// console.log(tex / xet);
-
-// concatenation
-
-// boolean
-// true or false
-
-var isABoy = true;
-var isAGirl = false;
-
-
-// reference variables
-// arrays
-// objects
-
-// arrays are zero indexed
-const arr = ["Cat", "Kangroo", "Polar Bear", "Hammer head Shark"];
-
-
-console.log(arr.length);
-console.log(arr[2]);
-
-// adding to array push/unshift
-// push adds to the end
-arr.push("Panda");
-console.log(arr);
-// unshift adds to the beginning
-arr.unshift("Lion");
-console.log(arr);
-
-
-// remove from the array
-// pop removes from the end
-arr.pop();
-console.log(arr);
-// shift removes from the beginning
-arr.shift();
-console.log(arr);
-
-// splice vs slice
-
-// // slice creates a copy of the array selection
-// const slicearr = arr.slice(1, 3);
-// console.log("arr", arr);
-// console.log("slicearr", slicearr);
-
-// // splice modified the array. remove
-// const splicearr = arr.splice(1, 3);
-// console.log("sp arr", arr);
-// console.log("splicearr", slicearr);
-
-// add item to the middle of the array
-// at index 1, remove 0 items and add "Elephant"
-arr.splice(1, 0, "Elephant");
-console.log(arr);
-// // remove item from the middle of the array
-// at index 1 remove 1 item
-arr.splice(3, 1, "brown bear");
-console.log(arr);
-
-// undefined if something doesnt exist
-// null of something exist but has not specified value
-// indexOf -1
-
-
-console.log("bird", arr.indexOf("Bird"));
-console.log("Elephant", arr.indexOf("Elephant"));
-
-// psycology
-
-// objects
-// key value pairs
-
-const newObj = {
-  name: "Daniel",
-  age: 20,
-  isABoy: true,
-  meta: {
-    height: 190,
-    weight: 80,
-    random: {
-      fc: "Madrid",
-      ff: "pounded yam and Egusi",
-      ftd: "mid night",
-      fc: "Black"
-    }
-  },
+for (let x = 0; x < tsArray.length; x++) {
+  thisArray.push(tsArray[x] + "a");
 }
 
-console.log("User name:", newObj.nickname)
-console.log("favourite color:", newObj.meta.random.fc)
+const filterArrray = [];
+const sortArray = [];
 
-let fire;
+console.log("after", tsArray);
+console.log("thisArray", thisArray);
 
-console.log(fire)
+// map through this array, for each item in this array
+const newArray = tsArray.map((item) => {
+  // console.log(item);
+  return item + "a";
+});
+
+console.log("newArray", newArray);
